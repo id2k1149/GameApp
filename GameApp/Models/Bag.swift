1ea133bff3cdce3b841b2c1ea0c53732a56facc5
id2k1149 <id2k1149@gmail.com>
2022-11-11 15:26:44 -0500

    edit Bag and func getRandomNumber()

--- GameApp/Models/Bag.swift
+++ GameApp/Models/Bag.swift
@@ -5,19 +5,21 @@
 //  Created by Max Franz Immelmann on 11/11/22.
 //
 
-import Foundation
-
 class Bag {
     static let shared = Bag()
     
-    var barrels = Array(1...99)
+    var barrels = Array(1...5)
     
     private init() {}
     
-    func getRandomNumber() -> Int {
-        let randomIndex = Int.random(in: 0..<barrels.count)
-        let randomNumber = barrels[randomIndex]
-        self.barrels.remove(at: randomIndex)
-        return randomNumber
+    func getNextBarrel() -> String {
+        if barrels.count == 0 {
+            return "Bag is empty"
+        } else {
+            let randomIndex = Int.random(in: 0..<barrels.count)
+            let randomNumber = barrels[randomIndex]
+            self.barrels.remove(at: randomIndex)
+            return randomNumber.formatted()
+        }
     }
 }
--- GameApp/ViewControllers/MainViewController.swift
+++ GameApp/ViewControllers/MainViewController.swift
@@ -10,27 +10,24 @@ import UIKit
 class MainViewController: UIViewController {
     
     // MARK: - @IBOutlets
-    @IBOutlet var randomNumberLabel: UILabel!
+    @IBOutlet var questionLabel: UILabel!
     @IBOutlet var yesButton: UIButton!
     @IBOutlet var noButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        randomNumberLabel.textColor = .blue
-        randomNumberLabel.font = .boldSystemFont(ofSize: 30)
 
         yesButton.layer.cornerRadius = 10
         noButton.layer.cornerRadius = 10
         
-        randomNumberLabel.text = getRandomNumber().formatted()
+        questionLabel.text = getRandomNumber()
     }
     
     // MARK: - @IBActions
     @IBAction func yesButtonTapped() {
     }
     @IBAction func noButtonTapped() {
-        randomNumberLabel.text = getRandomNumber().formatted()
+        questionLabel.text = getRandomNumber()
     }
     
 }
@@ -38,8 +35,13 @@ class MainViewController: UIViewController {
 // MARK: - extension
 extension MainViewController {
     
-    private func getRandomNumber() -> Int {
-        Bag.shared.getRandomNumber()
+    private func getRandomNumber() -> String {
+        let result = Bag.shared.getNextBarrel()
+        if result == "Bag is empty" {
+            return result
+        } else {
+            return "Do you have \(result) in your card?"
+        }
     }
     
 }
