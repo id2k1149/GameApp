7218d44dbc5f9c20b4df931dd885c725d5985822
id2k1149 <id2k1149@gmail.com>
2022-11-12 18:35:58 -0500

    edit main storyboard

--- GameApp/ViewControllers/MainViewController.swift
+++ GameApp/ViewControllers/MainViewController.swift
@@ -12,6 +12,38 @@ class MainViewController: UIViewController {
     // MARK: - @IBOutlets
     @IBOutlet var questionLabel: UILabel!
     
+    @IBOutlet var cardView: UIView!
+    
+    @IBOutlet var label_1_1: UILabel!
+    @IBOutlet var label_1_2: UILabel!
+    @IBOutlet var label_1_3: UILabel!
+    @IBOutlet var label_1_4: UILabel!
+    @IBOutlet var label_1_5: UILabel!
+    @IBOutlet var label_1_6: UILabel!
+    @IBOutlet var label_1_7: UILabel!
+    @IBOutlet var label_1_8: UILabel!
+    @IBOutlet var label_1_9: UILabel!
+    
+    @IBOutlet var label_2_1: UILabel!
+    @IBOutlet var label_2_2: UILabel!
+    @IBOutlet var label_2_3: UILabel!
+    @IBOutlet var label_2_4: UILabel!
+    @IBOutlet var label_2_5: UILabel!
+    @IBOutlet var label_2_6: UILabel!
+    @IBOutlet var label_2_7: UILabel!
+    @IBOutlet var label_2_8: UILabel!
+    @IBOutlet var label_2_9: UILabel!
+    
+    @IBOutlet var label_3_1: UILabel!
+    @IBOutlet var label_3_2: UILabel!
+    @IBOutlet var label_3_3: UILabel!
+    @IBOutlet var label_3_4: UILabel!
+    @IBOutlet var label_3_5: UILabel!
+    @IBOutlet var label_3_6: UILabel!
+    @IBOutlet var label_3_7: UILabel!
+    @IBOutlet var label_3_8: UILabel!
+    @IBOutlet var label_3_9: UILabel!
+    
     @IBOutlet var yesButton: UIButton!
     @IBOutlet var noButton: UIButton!
     
@@ -24,8 +56,6 @@ class MainViewController: UIViewController {
         questionLabel.text = getRandomNumber()
         
         playGame()
-        
-    
         
     }
     
